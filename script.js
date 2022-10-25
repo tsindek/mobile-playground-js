971c3a8e566a7618cf244286a70ed4e47996f0ae
Tobias Sindek <tobias.sindek@gmail.com>
2022-10-25 15:41:10 +0200

    feat: count squats through motiondetection

--- script.js
+++ script.js
@@ -1,41 +1,145 @@
-let px = 50; // Position x and y
-let py = 50;
-let vx = 0.0; // Velocity x and y
-let vy = 0.0;
-const updateRate = 1 / 60; // Sensor refresh rate
+const accelPermsButton = document.querySelector("#accelPermsButton");
+accelPermsButton.addEventListener("click", getAccelerationPermission);
+let latestEvent = null;
+let latestZEvent = null;
+let events = [];
+const accDisplay = document.querySelector(".displayAcc");
 
-function getAccel() {
-  DeviceMotionEvent.requestPermission().then((response) => {
-    if (response == "granted") {
-      // Add a listener to get smartphone orientation
-      // in the alpha-beta-gamma axes (units in degrees)
-      window.addEventListener("deviceorientation", (event) => {
-        // Expose each orientation angle in a more readable way
-        rotation_degrees = event.alpha;
-        frontToBack_degrees = event.beta;
-        leftToRight_degrees = event.gamma;
-
-        // Update velocity according to how tilted the phone is
-        // Since phones are narrower than they are long, double the increase to the x velocity
-        vx = vx + leftToRight_degrees * updateRate * 2;
-        vy = vy + frontToBack_degrees * updateRate;
-
-        // Update position and clip it to bounds
-        px = px + vx * 0.5;
-        if (px > 98 || px < 0) {
-          px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
-          vx = 0;
+function getAccelerationPermission() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === "granted") {
+          window.addEventListener("devicemotion", handleOrientation);
+        } else {
+          console.error("Request to access the orientation was rejected");
         }
-
-        py = py + vy * 0.5;
-        if (py > 98 || py < 0) {
-          py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
-          vy = 0;
-        }
-
-        dot = document.getElementsByClassName("indicatorDot")[0];
-        dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
-      });
-    }
-  });
+      })
+      .catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener("devicemotion", handleOrientation);
+  }
 }
+
+function handleOrientation(event) {
+  const y = event.acceleration.y;
+  latestEvent = y;
+}
+
+const touchField = document.querySelector(".touchField");
+touchField.addEventListener("touchstart", touchStart);
+touchField.addEventListener("touchend", touchEnd);
+
+let touchIntervall = null;
+let counterIntervall = null;
+
+function touchStart() {
+  if (touchIntervall === null && counterIntervall === null) {
+    touchIntervall = setInterval(whileTouchActive, 50);
+    //counterIntervall = setInterval(countSquats, 100);
+  }
+}
+
+function touchEnd() {
+  if (touchIntervall !== null) {
+    clearInterval(touchIntervall);
+    clearInterval(counterIntervall);
+    touchIntervall = null;
+    console.table(events);
+  }
+}
+
+let squatCounter = 0;
+
+accDisplay.textContent = squatCounter;
+
+function whileTouchActive() {
+  events.push(latestEvent.toFixed(2));
+  isSquat(latestEvent);
+}
+
+let phaseOne = false;
+let phaseTwo = false;
+let phaseThree = false;
+let phaseFour = false;
+
+function isSquat(value) {
+  if (value < 1) {
+    phaseOne = true;
+    //   console.log("phase one" + value);
+  }
+  if (phaseOne === true && value > 2) {
+    phaseTwo = true;
+    //   console.log("phase two" + value);
+  }
+  if (phaseOne === true && phaseTwo === true && value < 0) {
+    phaseThree = true;
+    //   console.log("phase three" + value);
+  }
+  if (
+    phaseOne === true &&
+    phaseTwo === true &&
+    phaseThree === true &&
+    value > 2
+  ) {
+    phaseFour = true;
+    console.log("phase four" + value);
+  }
+  if (
+    phaseOne === true &&
+    phaseTwo === true &&
+    phaseThree === true &&
+    phaseFour === true
+  ) {
+    //events = [];
+    phaseOne = false;
+    phaseTwo = false;
+    phaseThree = false;
+    phaseFour = false;
+    squatCounter++;
+  }
+  accDisplay.textContent = squatCounter;
+}
+
+// function countSquats() {
+//   for (let event of events) {
+//     if (event < 1) {
+//       phaseOne = true;
+//       //   console.log("phase one" + event);
+//     }
+//     if (phaseOne === true && event > 2) {
+//       phaseTwo = true;
+//       //   console.log("phase two" + event);
+//     }
+//     if (phaseOne === true && phaseTwo === true && event < 0) {
+//       phaseThree = true;
+//       //   console.log("phase three" + event);
+//     }
+//     if (
+//       phaseOne === true &&
+//       phaseTwo === true &&
+//       phaseThree === true &&
+//       event > 2
+//     ) {
+//       phaseFour = true;
+//       console.log("phase four" + event);
+//     }
+//     if (
+//       phaseOne === true &&
+//       phaseTwo === true &&
+//       phaseThree === true &&
+//       phaseFour === true
+//     ) {
+//       //events = [];
+//       phaseOne = false;
+//       phaseTwo = false;
+//       phaseThree = false;
+//       phaseFour = false;
+//       squatCounter++;
+//       return;
+//     }
+//     accDisplay.textContent = squatCounter;
+//   }
+// }
